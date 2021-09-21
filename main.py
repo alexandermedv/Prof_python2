4715665107b2be6bf5055b42ffac3951e82a9d8b
Alexander Medvedev <medvedevae@pgkweb.ru>
2021-09-22 01:13:02 +0300

    minor changes

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from pprint import pprint
-# читаем адресную книгу в формате CSV в список contacts_list
 import csv
 import re
 import pandas as pd
@@ -24,6 +22,7 @@ def lastname(contacts_list):
 
     return result
 
+
 def firstname(contacts_list):
     """Обработка имени"""
 
@@ -37,6 +36,7 @@ def firstname(contacts_list):
         result.append(record)
 
     return result
+
 
 def phone(contacts_list):
     """Обработка номера телефона"""
@@ -56,41 +56,15 @@ def phone(contacts_list):
 
     return result
 
-def deduplicate(contacts_list):
-    """Объединение дубликатов"""
-
-    pattern = "[\d]"
-    result = []
-    for record in contacts_list:
-        phone_list = re.findall(pattern, record[5])
-        if phone_list:
-            if phone_list[0] == '8':
-                phone_list[0] = '7'
-            if len(phone_list) > 11 and re.findall('доб.', record[5]):
-                record[5] = '+' + phone_list[0] + '(' + ''.join(phone_list[1:4]) + ')' + ''.join(phone_list[4:7]) + '-' + ''.join(phone_list[7:9]) + '-' + ''.join(phone_list[9:11]) + ' доб.' + ''.join(phone_list[11:])
-            else:
-                record[5] = '+' + phone_list[0] + '(' + ''.join(phone_list[1:4]) + ')' + ''.join(phone_list[4:7]) + '-' + ''.join(phone_list[7:9]) + '-' + ''.join(phone_list[9:11])
-        result.append(record)
-
-    return result
 
 if __name__ == "__main__":
-
 
     with open("phonebook_raw.csv") as f:
         rows = csv.reader(f, delimiter=",")
         contacts_list = list(rows)
-    # pprint(contacts_list)
 
-    # TODO 1: выполните пункты 1-3 ДЗ
-    # ваш код
     contacts_list = lastname(contacts_list)
     contacts_list = firstname(contacts_list)
     contacts_list = phone(contacts_list)
     contacts_list = pd.DataFrame(contacts_list).replace('', np.NaN).groupby([0, 1]).first()
-
-    print(contacts_list)
-
-    # TODO 2: сохраните получившиеся данные в другой файл
-    # код для записи файла в формате CSV
     contacts_list.to_csv("phonebook.csv", header=False)
